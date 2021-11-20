485026543270463f58f155adf7c32339d9fbb030
Igor <ikkniazev@yandex.ru>
2021-11-20 16:21:54 +0300

    COMMENTS

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ async function getInfo() {
   const infoArray = [
     await fetch('http://jsonplaceholder.typicode.com/users'),
     await fetch('http://jsonplaceholder.typicode.com/posts'),
+    await fetch('http://jsonplaceholder.typicode.com/posts/2/comments')
   ]
   const data = await Promise.all(infoArray)
   const usersArray = await data[0].json();
   const postsArray = await data[1].json();
+  const commentsArray = await data[2].json();
   const newUsersArray = usersArray.map((el, i) => {
     return el = {
       id: el.id,
@@ -16,11 +18,31 @@ async function getInfo() {
       address: `${el.address.city}, ${el.address.street}, ${el.address.suite}`,
       website: `https://${el.website}`,
       company: el.company.name,
-      posts: postsArray.filter(el => el.userId === (i + 1)).map(el => el = {id: el.id, title: el.title, title_crop: `${el.title.slice(0, 20)}...`, body: `${el.body}`})
+      posts: postsArray
+        .filter((el) => el.userId === (i + 1))
+        .map((el, i) => {
+          return el = { id: el.id, title: el.title, title_crop: `${el.title.slice(0, 20)}...`, body: `${el.body}`};
+        })
+
     }
   })
-  console.log(newUsersArray[1].posts);
+  const ervin = newUsersArray.filter(el => el.name === "Ervin Howell")
+// console.log(ervin[0].posts);
+  
+console.log(commentsArray);
+  // console.log(newUsersArray[1].posts);
 
 }
 
-getInfo()
+getInfo();
+
+// async function comments() {
+//   const res = await fetch(`http://jsonplaceholder.typicode.com/posts/${i}/comments`)
+//   const data = await res.json()
+//   const obj = await getInfo()
+//   console.log(obj);
+//   obj.map(el => el.posts)
+//   // return obj
+// }
+
+// comments();
