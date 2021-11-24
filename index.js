c28d1ef2e0007d96729e59f64b04182c55338f55
Igor <ikkniazev@yandex.ru>
2021-11-24 13:53:40 +0300

    DONE

--- index.js
+++ index.js
@@ -4,45 +4,43 @@ async function getInfo() {
   const infoArray = [
     await fetch('http://jsonplaceholder.typicode.com/users'),
     await fetch('http://jsonplaceholder.typicode.com/posts'),
-    await fetch('http://jsonplaceholder.typicode.com/posts/2/comments')
   ]
   const data = await Promise.all(infoArray)
   const usersArray = await data[0].json();
   const postsArray = await data[1].json();
-  const commentsArray = await data[2].json();
-  const newUsersArray = usersArray.map((el, i) => {
-    return el = {
+  const newUsersArray = await usersArray.map((el, i) => {
+    return obj1 = {
       id: el.id,
       name: el.name,
       email: el.email,
       address: `${el.address.city}, ${el.address.street}, ${el.address.suite}`,
       website: `https://${el.website}`,
       company: el.company.name,
-      posts: postsArray
+      posts: [],
+      posts1: postsArray
         .filter((el) => el.userId === (i + 1))
-        .map((el, i) => {
-          return el = { id: el.id, title: el.title, title_crop: `${el.title.slice(0, 20)}...`, body: `${el.body}`};
+        .map(async (el, i) => {
+          
+          const response = await fetch(`http://jsonplaceholder.typicode.com/posts/${i}/comments`);
+          const data = await response.json();
+          el = { id: el.id, title: el.title, title_crop: `${el.title.slice(0, 20)}...`, body: `${el.body}`, comments: data };
+          return el;
         })
-
+        .map((el) => el
+          .then(data => {
+            newUsersArray[i].posts.push(data)
+          })
+          .catch(err => err)
+        )
     }
   })
-  const ervin = newUsersArray.filter(el => el.name === "Ervin Howell")
-// console.log(ervin[0].posts);
-  
-console.log(commentsArray);
-  // console.log(newUsersArray[1].posts);
+
+  for (let i = 0; i < newUsersArray.length; i = i + 1) {
+    const some = await Promise.all(newUsersArray[i].posts1)
+    delete newUsersArray[i].posts1
+  }
+
+  console.log(newUsersArray);
 
 }
-
 getInfo();
-
-// async function comments() {
-//   const res = await fetch(`http://jsonplaceholder.typicode.com/posts/${i}/comments`)
-//   const data = await res.json()
-//   const obj = await getInfo()
-//   console.log(obj);
-//   obj.map(el => el.posts)
-//   // return obj
-// }
-
-// comments();
